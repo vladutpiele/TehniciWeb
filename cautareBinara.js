0f621598e7562601511ee23bcf737cc39c296fc5
vladutpiele <82233734+vladutpiele@users.noreply.github.com>
2024-06-10 00:15:18 +0300

    Update cautareBinara.js

--- cautareBinara.js
+++ cautareBinara.js
@@ -2,19 +2,29 @@ let buttonCodProblema = document.querySelector("#codProblema");
 let buttonAlteProbleme = document.querySelector("#alteProbleme");
 let afiseazaCod = document.querySelector("#afiseazaDupaClickCodProblema");
 let afiseazaProblemeAsemanatoare = document.querySelector("#problemeAsemanatoare");
+let containers = document.getElementsByClassName('container');
+
+afiseazaCod.style.display = 'none';
+afiseazaProblemeAsemanatoare.style.display = 'none';
 
 buttonCodProblema.addEventListener('click', () => {
-    if (afiseazaCod.style.display == 'none') {
+    if (afiseazaCod.style.display === 'none' || afiseazaCod.style.display === '') {
         afiseazaCod.style.display = 'block';
+        for (let i = 0; i < containers.length; i++) {
+            containers[i].style.display = 'none';
+        }
     } else {
         afiseazaCod.style.display = 'none';
+        for (let i = 0; i < containers.length; i++) {
+            containers[i].style.display = 'block';
+        }
     }
-})
+});
 
 buttonAlteProbleme.addEventListener('click', () => {
-    if (afiseazaProblemeAsemanatoare.style.display == 'none') {
+    if (afiseazaProblemeAsemanatoare.style.display === 'none' || afiseazaProblemeAsemanatoare.style.display === '') {
         afiseazaProblemeAsemanatoare.style.display = 'block';
     } else {
         afiseazaProblemeAsemanatoare.style.display = 'none';
     }
-})
+});
